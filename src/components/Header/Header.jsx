f208d2bc22deda662b768cb57cfc65d18b5b92fd
natalia mohyla <mogilesha81@gmail.com>
2026-08-12 19:25:36 +0200

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,24 @@ import styles from './Header.module.css';
 import { useNavigate } from "react-router";
 
 const Header = () => {
-    
-    return ( );
-
+    const navigate = useNavigate()
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <img src='/public/images/ukraine (2).svg' alt="Logo" className={styles.ukraine} />
+                <img src="/public/images/LearnLingo.svg" alt="Learnlingo" className={styles.lingo} />
+            </div>
+            <nav className={styles.nav}>
+                <NavLink to='/' className={({ isActive }) => isActive ? styles.active : ''}>Home</NavLink>
+                <NavLink to='/teachers' className={({ isActive }) => isActive ? styles.active : ''}>Teachers</NavLink>
+            </nav>
+            <div className={styles.blockbutton}>
+                <button type="button" className={styles.login} onClick={() => navigate('/login')}>LogIn
+                    <img src="/public/images/log-in-01.png" alt="Login" className={styles.icon } /></button>
+                <button type="button" className={styles.button} onClick={() => navigate('/registration')}>Registration</button>
+                
+            </div>
+        </header>
+    );
 };
 export default Header;
